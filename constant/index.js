e616f0ebd63481ca7f053c4d23384bc7ab89653f
franki <8024ldbryant@gmail.com>
2023-10-30 20:08:24 +0800

    feat: fixed menu deal with addition

---
+++ constant/index.js
@@ -0,0 +1,36 @@
+const DEFAULT_LIST_NAMES = [
+  'MY Day', // 1
+  'Important', // 2
+  'Planned', // 3
+  'Assigned to me', // 4
+  'Tasks', // 5
+];
+
+const MY_DAY = 1;
+const IMPORTANT = 2;
+const PLANNED = 3;
+const ASSIGNED_TO_ME = 4;
+const TASKS = 5;
+
+const DEFAULT_LIST_IDS = [
+  MY_DAY, // MY Day
+  IMPORTANT, // Important
+  PLANNED, // Planned
+  ASSIGNED_TO_ME, // Assigned to me
+  TASKS, // Tasks
+];
+
+const ADDED_MY_DAY = 1;
+const MARKED_AS_IMPORTANT = 1;
+
+module.exports = {
+  DEFAULT_LIST_NAMES,
+  MY_DAY,
+  IMPORTANT,
+  PLANNED,
+  ASSIGNED_TO_ME,
+  TASKS,
+  DEFAULT_LIST_IDS,
+  MARKED_AS_IMPORTANT,
+  ADDED_MY_DAY,
+};
